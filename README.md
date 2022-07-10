adbad38add99d51ec243e3c957f11c4e176037a7
JOSE RAMON ESCARPA PEREZ <joseramon.escarpa@hotmail.es>
2022-07-10 19:35:04 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# wmedia
+Ejemplo Curso de Wmedia con alguna modificación. Curso de Juan Andrés Núñez.
+Probando git, ESLint y más cosillas
---
+++ app.js
@@ -0,0 +1,17 @@
+// seleccionamos los elementos html que necestitamos
+var entrada = document.getElementById('tarea');
+var boton = document.getElementById('boton');
+var tareas = document.querySelector('ul');
+
+// cuando se haga click en el boton
+boton.addEventListener('click', () => {
+  var tarea = entrada.value;
+  var li = document.createElement('li');
+  li.textContent = tarea;
+  tareas.appendChild(li);
+  // document.getElementById('tarea').innerHTML = '';
+  entrada.value = '';
+  //obtener el ultimo elemento de una lista
+  var ultimo = tareas.lastElementChild;
+  console.log(ultimo);
+});
