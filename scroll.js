16d5b632f846818fba5e16b512fe7421b31676a4
JOSE RAMON ESCARPA PEREZ <joseramon.escarpa@hotmail.es>
2023-02-11 22:13:33 +0100

    botones4

--- scroll.js
+++ scroll.js
@@ -1,10 +1,6 @@
 var top = document.getElementById('top');
-top.addEventListener('click', () => {
-  window.scrollTo(0, 0);
-});
+top.addEventListener('click', () => {window.scrollTo(0, 0);});
 
 //boton para cerrar el navegador
 var cerrar = document.getElementById('cerrar');
-cerrar.addEventListener('click', () => {
-  window.close();
-});
+cerrar.addEventListener('click', () => { window.close(); });
