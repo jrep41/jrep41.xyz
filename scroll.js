7575e201d87b39b6a43bfea4b4425c2ccd6a24f0
JOSE RAMON ESCARPA PEREZ <joseramon.escarpa@hotmail.es>
2023-02-16 15:09:36 +0100

    mio5

--- scroll.js
+++ scroll.js
@@ -1,5 +1,17 @@
+
+
+
 var top = document.getElementById('top');
 top.addEventListener('click', () => {window.scrollTo(0, 0);});
+
+
+
+
+//boton para abrir una nueva ventana con un calendario
+var calendario = document.getElementById('calendario');
+calendario.addEventListener('click', () => { window.open('https://calendar.google.com/calendar/r'); });
+
+
 
 //boton para cerrar el navegador
 var cerrar = document.getElementById('cerrar');
